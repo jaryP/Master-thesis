fc531340d0c72c8e9868ad9d6d6228a4a2fb393a
jary <pomponi>
2019-03-09 02:04:08 +0100

    first commit
    Reworked and working code

---
+++ networks/Kafnet.py
@@ -0,0 +1,38 @@
+from networks.ActivationFunctions import KAF, elu
+import torch.nn as nn
+
+class KAFCNN(nn.Module):
+
+    def __init__(self, num_tasks):
+        super(KAFCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.maxpool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(32, 52, kernel_size=3, padding=1, stride=2)
+        self.batchnorm2 = nn.BatchNorm2d(52)
+        self.conv3 = nn.Conv2d(52, 52, kernel_size=3, padding=1)
+        self.batchnorm3 = nn.BatchNorm2d(52)
+        self.conv4 = nn.Conv2d(52, 96, kernel_size=3, padding=1, stride=2)
+        self.batchnorm4 = nn.BatchNorm2d(96)
+        self.kaf1 = KAF(32, init_fcn=elu, is_conv=True, D=15)
+        self.kaf2 = KAF(52, init_fcn=elu, is_conv=True, D=15)
+        self.kaf3 = KAF(52, init_fcn=elu, is_conv=True, D=15)
+        self.kaf4 = KAF(96, init_fcn=elu, is_conv=True, D=15)
+        # self.kaf5 = KAF(96, init_fcn=elu, is_conv=True)
+        self.output = nn.Linear(1536, num_tasks * 2)
+        self.current_task = 0
+
+    def forward(self, input):
+        x = self.kaf1(self.batchnorm1(self.conv1(input)))
+        x = self.kaf2(self.batchnorm2(self.conv2(x)))
+        x = self.maxpool(self.kaf3(self.batchnorm3(self.conv3(x))))
+        x = self.kaf4(self.batchnorm4(self.conv4(x)))
+        # x = self.maxpool(self.kaf5(self.batchnorm5(self.conv5(x))))
+        x = self.output(x.reshape(input.shape[0], -1))
+        return x[:, self.current_task * 2:self.current_task * 2 + 2]
+
+    def set_task(self, task):
+        self.current_task = task
+
+    def eval_forward(self, x):
+        return (nn.functional.softmax(x, dim=1).max(dim=1)[1]).cpu().detach().numpy()
---
+++ networks/continual_learning.py
@@ -0,0 +1,80 @@
+import torch.nn as nn
+from copy import deepcopy
+from configs.configs import DefaultConfig
+from utils.datasetsUtils.dataset import GeneralDatasetLoader
+import torch.nn.functional as F
+
+
+class EWC(object):
+    # An utility object for computing the EWC penalty
+
+    def __init__(self, model: nn.Module, dataset: GeneralDatasetLoader, config: DefaultConfig):
+        # model: the model (either initialized or pretrained on previous tasks)
+        # dataset: a list of (x, task_idx) elements from the previous tasks
+        # device: current device in use
+        # config: a configuration dictionary (see /configs/)
+
+        self.model = model
+
+        self.is_conv = config.IS_CONVOLUTIONAL
+        self.device = config.DEVICE
+
+        # Save trainable parameters of the model in a dictionary
+        self.params = {n: p for n, p in self.model.named_parameters() if p.requires_grad}
+
+        # Get means and precisions
+        self._means = {}
+        for n, p in deepcopy(self.params).items():
+            self._means[n] = p.to(self.device)
+
+        self._precision_matrices = None
+
+    def __call__(self, *args, **kwargs):
+        self._diag_fisher(old_tasks=kwargs['old_tasks'])
+        return self
+
+    def _diag_fisher(self, old_tasks):
+        # Initialize precision matrices
+        precision_matrices = {}
+        for n, p in deepcopy(self.params).items():
+            p.data.zero_()
+            precision_matrices[n] = p.data
+
+        self.model.eval()
+
+        for (input, task_idx) in old_tasks:
+
+            self.model.zero_grad()
+            self.model.set_task(task_idx)
+            input = input.to(self.device)
+
+            if self.is_conv:
+                output = self.model(input.view(1, input.shape[0], input.shape[1], input.shape[2]))
+            else:
+                output = self.model(input.view(1, -1))
+
+            label = output.max(1)[1].view(-1)
+            loss = F.nll_loss(F.log_softmax(output, dim=1), label)
+            loss.backward()
+
+            for n, p in self.model.named_parameters():
+                precision_matrices[n].data += p.grad.data ** 2 / len(old_tasks)
+
+        precision_matrices = {n: p for n, p in precision_matrices.items()}
+
+        self._precision_matrices = precision_matrices
+
+    def penalty(self, model: nn.Module):
+        loss = 0
+        if self._precision_matrices is None:
+            return 0
+
+        for n, p in model.named_parameters():
+
+            # print(self._precision_matrices[n])
+            # print(self._means[n])
+
+            _loss = self._precision_matrices[n] * (p.to(self.device) - self._means[n]) ** 2
+            loss += _loss.sum()
+
+        return loss
