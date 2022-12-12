12476d7a1c4b60f72550dabca889e911dd393bb9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 19:09:15 +0000

    Adding tarfile member sanitization to extractall()

--- utils/datasetsUtils/CIFAR.py
+++ utils/datasetsUtils/CIFAR.py
@@ -197,7 +197,29 @@ class Cifar10(utils.datasetsUtils.dataset.GeneralDatasetLoader):
             shutil.copyfileobj(response, out_file)
 
         with tarfile.open(join(self.download_path, self.filename), "r:gz") as tar:
-            tar.extractall(path=self.folder)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=self.folder)
 
 
 class Cifar100(Cifar10):
